db090caf52f6fee1d01aa46868a1f85b8172bc74
Arsenij390 <152289286+Arsenij390@users.noreply.github.com>
2025-04-20 16:08:27 +0500

    ProjectOnUnity6

--- Main/Interaction/NPC_Text/MassiveOwn.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MassiveOwn", menuName = "Dialogue/MassiveOwn")]
-public class MassiveOwn : ScriptableObject
-{
-    public SignMassive[] signGet;
-}
