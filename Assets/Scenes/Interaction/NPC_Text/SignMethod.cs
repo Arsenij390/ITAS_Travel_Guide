37ff33d918b0ed7a9d1eaa1c9a6064b1222d4cb1
Arsenij390 <152289286+Arsenij390@users.noreply.github.com>
2025-04-20 16:27:22 +0500

    AddedScripts

---
+++ Assets/Scenes/Interaction/NPC_Text/SignMethod.cs
@@ -0,0 +1,25 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class SignMethod : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI signText;
+    [SerializeField] private GameObject signObject;
+    [SerializeField] private ScriptableObject signMassive;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public IEnumerator CurrentDialogue(MassiveOwn dialogueObject)
+    {
+        for(int i = 0; i < dialogueObject.signGet.Length; i++)
+        {
+            signText.text = dialogueObject.signGet[i].dialogue;
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Mouse0));
+
+            yield return null;
+        }
+
+        signObject.SetActive(false);
+    }
+}
