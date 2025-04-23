44a002c8e04143e33863fbd516c5cfff5caf4d6a
Arsenij390 <152289286+Arsenij390@users.noreply.github.com>
2025-04-24 00:23:54 +0500

    BugFix for distance logic
    Исправлена ошибка, когда игрок находясь в определенной позиции по Z не видел текст.

--- ThisProject/Assets/Scenes/Interaction/NPC_Text/FlyText.cs
+++ ThisProject/Assets/Scenes/Interaction/NPC_Text/FlyText.cs
@@ -10,7 +10,7 @@ public class FlyText : MonoBehaviour
     [SerializeField] private bool procLog;
     [SerializeField] private bool colorLog;
 
-    [Range(0f, 2f)] public float chanchingTheColor;
+    [Range(0f, 3f)] public float chanchingTheColor;
     Color colText;
     Color colBckImg;
 
@@ -24,7 +24,7 @@ public class FlyText : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        chanchingTheColor = 2;
+        chanchingTheColor = 1;
         colText.a = 255;
         colBckImg.a = 255;
         colBckImg = Color.black;
@@ -35,53 +35,37 @@ public class FlyText : MonoBehaviour
     {
         colorDIs.transform.LookAt(player.transform);
         colorDIs.transform.Rotate(0, 180, 0);
-        double PlayerZ = Math.Sqrt(player.transform.position.z * player.transform.position.z);
-        float rasniza = (colorDIs.transform.localPosition.z - Convert.ToSingle(PlayerZ)) + 3.5f;
-        float rasnizaClose = (colorDIs.transform.localPosition.z - player.transform.position.z);
+        float distance = Vector3.Distance(colorDIs.transform.position, player.transform.position);
+        Debug.Log($"distance: {distance}");
 
-        if (rasnizaLog)
-        {
-            Debug.Log($"ras: {rasniza}");
-            Debug.Log($"rasC: {rasnizaClose}");
-        }
-
-        if (rasniza < 2 || rasniza == 2)
+        if (distance < 4 && distance > 1)
         {
             try
             {
-                procBeg = (((float)chanchingTheColor - rasniza) * 100) / 2;
+                procBeg = ((((float)chanchingTheColor - (distance - 1)) * 100) / 2) * (-1);
+                Debug.Log(procBeg);
                 procEnd = Convert.ToByte((255 * (100 - procBeg) / 100));
+                Debug.Log(procEnd);
 
-                if (procLog)
-                {
-                    Debug.Log($"Beg: {procBeg}");
-                    Debug.Log($"End: {procEnd}");
-                }
-
-                if (procBeg > 90)
-                {
-                    colText = new Color32(255, 255, 255, 0);
-                    flyText.GetComponent<TextMeshPro>().faceColor = colText;
-                    backImg.GetComponent<SpriteRenderer>().color = colText;
-                }
-                else
-                {
-                    colText = new Color32(255, 255, 255, procEnd);
-                    colBckImg = new Color32(30, 30, 50, procEnd);
-                    flyText.GetComponent<TextMeshPro>().faceColor = colText;
-                    backImg.GetComponent<SpriteRenderer>().color = colBckImg;
-                }
-
-                if (colorLog)
-                {
-                    Debug.Log(colText);
-                }
+                colText = new Color32(255, 255, 255, procEnd);
+                colBckImg = new Color32(30, 30, 50, procEnd);
+                flyText.GetComponent<TextMeshPro>().faceColor = colText;
+                backImg.GetComponent<SpriteRenderer>().color = colBckImg;
             }
             catch (Exception e)
             {
-                colText = new Color32(255, 255, 255, 0);
+                colText = new Color32(255, 255, 255, 255);
+                colBckImg = new Color32(30, 30, 50, 255);
+                flyText.GetComponent<TextMeshPro>().faceColor = colText;
+                backImg.GetComponent<SpriteRenderer>().color = colBckImg;
                 //Debug.Log("Œÿ»¡ ¿");
             }
+        }
+        else
+        {
+            colText = new Color32(255, 255, 255, 0);
+            flyText.GetComponent<TextMeshPro>().faceColor = colText;
+            backImg.GetComponent<SpriteRenderer>().color = colText;
         }
     }
 }
